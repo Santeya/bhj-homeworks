e1e4be29a2932bfb123ca07639a365449c5611d2
Santeya <santeya@gmx.net>
2022-09-26 16:33:58 +0100

    Homework 7 update

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -2,42 +2,21 @@ const progress = document.getElementById('progress');
 
 const url = 'https://netology-slow-rest.herokuapp.com/upload.php';
 
-const xhr = new XMLHttpRequest();
-/*
-const handler = function () {
-    
-    if (xhr.upload.loadstart) {
+document.getElementById('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const xhr = new XMLHttpRequest();
+    xhr.upload.onloadstart = () => {
         progress.value = 0.2;
-    } else if (xhr.upload.progress) {
-        progress.value = 0.5;
-    } else if (xhr.upload.load) {
+    }
+    xhr.upload.onprogress = () => {
         progress.value = 0.7;
-    } else if (xhr.upload.loadend) {
+    }
+    xhr.upload.onload = () => {
         progress.value = 1;
     }
-}*/
 
-xhr.upload.onloadstart = () => {
-    progress.value = 0.2;
-}
+    xhr.open('POST', url);
 
-xhr.upload.onprogress = () => {
-    progress.value = 0.7;
-}
-
-xhr.upload.onload = () => {
-    progress.value = 1;
-}
-/*
-xhr.addEventListener('upload', handler)
-*/
-
-xhr.open('POST', url);
-
-
-const formData = new FormData(document.getElementById('form'));
-const file = document.getElementById('file').file;
-
-formData.append('file', file)
-
-xhr.send(formData);
+    const formData = new FormData(document.getElementById('form'));
+    xhr.send(formData);
+});
